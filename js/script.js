43899052ec80c3d73b9b3213d7022f1cee563114
EmoticonDepot <emoticondepot@webname.com>
2022-05-08 20:17:39 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ let timerOne, timerTwo;
 function copyEvent(id){
     var str = document.getElementById(id);
     window.getSelection().selectAllChildren(str);
-    document.execCommand("Copy");
+    document.execCommand("copy");
 
     toast.classList.add("active");
     progress.classList.add("active");
